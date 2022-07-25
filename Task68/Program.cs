ba3db5d007e7ba5afc781d9f9208437702a62b94
Streltsov_Mike <streltsovmike@yandex.ru>
2022-07-26 00:10:56 +0300

    Add HomeWork

--- Task68/Program.cs
+++ Task68/Program.cs
@@ -3,18 +3,21 @@
 
 
 
+int Akkerman(int N, int M)
+{
+    if (N == 0) return M + 1;
+    if (N > 0 && M == 0) return (Akkerman(N - 1, 1));
+    return (Akkerman(N - 1, Akkerman(N, M - 1)));
+}
+
 Console.Write("Введите число N: ");
 int N = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число M: ");
 int M = Convert.ToInt32(Console.ReadLine());
 
 
-int Akkerman(int N, int M)
-{
-    if (N == 0) return M + 1;
-    if (N > 0 && M == 0) return (Akkerman(N - 1, 1));
-    if (N > 0 && M > 0) return (Akkerman(N - 1, Akkerman(N, M - 1)));
-    return Akkerman(N, M);
-}
-
+if (N>=0 && M>=0)
 Console.WriteLine(Akkerman(N, M));
+
+else
+Console.WriteLine("Числа должны быть натураньными");
